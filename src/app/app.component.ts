b15c8158634aae9f47de0c82becde4b4db00b0a5
Alexey Parkhomyuk <parkhomyukalex@gmail.com>
2016-12-05 01:20:29 +0200

    1 step add Markers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  zoom:  number=10;
   lat: number = 32.08088;
   lng: number = 34.78057;
+
+  markers:marker[]=[
+    {
+      name:'Instructors 24',
+      lat: 32.81841,
+      lng:  34.9885,
+      draggable:true
+    },
+    {
+      name:'Instructors 105',
+      lat: 31.79213 ,
+      lng: 34.64966,
+      draggable:true
+    },
+    {
+      name:'Instructors 209',
+      lat: 31.76904  ,
+      lng: 35.21633,
+      draggable:true
+    },
+    {
+      name:'Instructors 4',
+      lat: 32.81149,
+      lng:  35.11323,
+      draggable:true
+    }
+
+
+  ];
+
+  constructor(){}
 }
+// Marker Type
+interface marker{
+  name?:string;
+  lat: number;
+  lng: number;
+  draggable:boolean;
+}
+
