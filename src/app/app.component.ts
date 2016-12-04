fb6341e5ff0cb372733de3cca62566d1c1774ec0
Alexey Parkhomyuk <parkhomyukalex@gmail.com>
2016-12-05 00:41:53 +0200

    0 step Basis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  lat: number = 32.08088;
+  lng: number = 34.78057;
 }
