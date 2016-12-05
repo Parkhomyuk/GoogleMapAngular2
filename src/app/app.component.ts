249852469daa6f890a8bd23488215a8dc1e95f14
Alexey Parkhomyuk <parkhomyukalex@gmail.com>
2016-12-05 12:14:32 +0200

    5 Final step  remove and update markers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,7 +53,7 @@ export class AppComponent {
 
     var newLat = $event.coords.lat;
     var newLng = $event.coords.lng;
-
+    this._markerService.updateMarker(updMarker, newLat, newLng);
   }
 
   addMarker(){
@@ -74,6 +74,17 @@ export class AppComponent {
     this.markers.push(newMarker);
     this._markerService.addMarker(newMarker);
   }
+  removeMarker(marker){
+    console.log('Removing Marker...');
+    for(var i = 0;i < this.markers.length;i++){
+      if(marker.lat == this.markers[i].lat && marker.lng == this.markers[i].lng){
+        this.markers.splice(i, 1);
+      }
+    }
+
+    this._markerService.removeMarker(marker);
+  }
+
 }
 
 // Marker Type
--- src/app/services/marker.service.ts
+++ src/app/services/marker.service.ts
@@ -23,5 +23,28 @@ export class MarkerService extends Init{
     localStorage.setItem('markers', JSON.stringify(markers));
   }
 
+  updateMarker(marker, newLat,newLng){
+    var markers= JSON.parse(localStorage.getItem('markers'));
+    for(var i=0;i<markers.length;i++){
+      if(marker.lat==markers[i].lat&&marker.lng==markers[i].lng){
+        markers[i].lat= newLat;
+        markers[i].lng= newLng;
+      }
+    }
+    localStorage.setItem('markers', JSON.stringify(markers));
+  }
+  removeMarker(marker){
+    // Fetch markers
+    var markers = JSON.parse(localStorage.getItem('markers'));
+
+    for(var i = 0;i < markers.length;i++){
+      if(marker.lat == markers[i].lat && marker.lng == markers[i].lng){
+        markers.splice(i, 1);
+      }
+    }
+
+    // Set ls markers again
+    localStorage.setItem('markers', JSON.stringify(markers));
+  }
 
 }
